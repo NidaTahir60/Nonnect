a5138569b83e274fac57b717b282bd6ae50f494b
NidaTahir60 <139445464+NidaTahir60@users.noreply.github.com>
2024-10-01 12:43:10 +0500

    Update style.blade.php

--- resources/views/planning_portal/partials/style.blade.php
+++ resources/views/planning_portal/partials/style.blade.php
@@ -2,13 +2,13 @@
 <html class="no-js" lang="en">
 
 <head>
-    <title>Ateeb Cosmetics</title>
+    <title>Plan Connect</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="author" content="ThemeZaa">
+  
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <meta name="description"
-        content="Ateeb Cosmetics PVT LTD">
+        content="Plan Connect">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- favicon icon -->
